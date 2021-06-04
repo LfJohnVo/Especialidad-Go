691906da0ce3486701b41199b2412fc7d1eda0e8
John <vojohn95@gmail.com>
2021-06-04 01:42:28 -0500

    Comentarios

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 <h3>Curso 1: Go desde de 0</h3>
 <ol>
     <li>Variables</li>
-    
+    <li>Constantes</li>
+    <li></li>
 </ol>
---
+++ godesdecero/3-comentarios/main.go
@@ -0,0 +1,19 @@
+///Package main es el paquete principal
+//que me permite ejecutar mi aplicación
+package main
+
+//Test de una variable de prueba
+var Test = 123
+
+//Pi es una constante
+const Pi = 3.14
+
+/*
+Comentario multilinea
+ */
+
+//Generar documentación en terminal con: go doc --all
+
+func main(){
+
+}
