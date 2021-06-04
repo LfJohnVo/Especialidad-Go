8a38cdbc3b9e6397fd271ca0ee3922eba3ca8f90
John <vojohn95@gmail.com>
2021-06-04 14:01:59 -0500

    Slice part 2

--- godesdecero/8-slices/main.go
+++ godesdecero/8-slices/main.go
@@ -20,8 +20,19 @@ func main(){
 	animals := set[0:5]
 	fly := set[3:7]
 	fly[0] = "🦅"
+	fly = append(fly, "🍍")
+	//len numero de elementos en el slice
+	//cap numero de elementos del array donde apunta el slice, a partir del indice de donde se creo el slice
+	//append agregar elementos al slice
+	//make devuelve un slice de stings
+	//fly := make([]string , 0 , 3)
+
 	fmt.Println("Array: ", set)
 	fmt.Println("Animales: ",animals)
 	fmt.Println("Voladores: ",fly[0])
 	fmt.Println("ALL: ", set[:])
+	fmt.Println("Tamaño: ", len(fly))
+	fmt.Println("Capacidad: ", cap(fly))
+	fmt.Println("Tamaño: ", len(fly))
+	fmt.Println(fly == nil)
 }
